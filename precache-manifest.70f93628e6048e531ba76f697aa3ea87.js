136cd4291aad749a1c0809f1df371dfff5d2f4eb
Robert Grow <rgrow@growtechinc.com>
2021-04-28 17:26:11 -0400

    Updates

---
+++ precache-manifest.70f93628e6048e531ba76f697aa3ea87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6421d51d1e137d4a091b397730467aa",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "716fc7d698dc19293b1b",
+    "url": "/monsters-rolodex/static/css/main.62d87302.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "716fc7d698dc19293b1b",
+    "url": "/monsters-rolodex/static/js/main.05b58826.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
